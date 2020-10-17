0474344ab91fffdf51e30c5a617f5080f9b822bf
bobo mallick <mallickbobo@gmail.com>
2020-10-18 00:01:38 +0530

    client side

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAT0hBkNwoI4jUKyAL_KyYWP_7Hj8-RLkU",
+  authDomain: "creative-agency3.firebaseapp.com",
+  databaseURL: "https://creative-agency3.firebaseio.com",
+  projectId: "creative-agency3",
+  storageBucket: "creative-agency3.appspot.com",
+  messagingSenderId: "604348336266",
+  appId: "1:604348336266:web:926e666b2904bde2af8573"
+};
+export default firebaseConfig;
